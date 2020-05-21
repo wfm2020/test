c8d10ae07f51e102b4342479429eee46845f01c3
Angelos Arampatzis <angelos.arabatzis@gmail.com>
2020-05-21 14:37:13 +0200

    initial commit

---
+++ src/main/java/test/Application.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class Application {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @EventListener
+    private void processPostDeploy(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("Process_0cz4jia");
+    }
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
