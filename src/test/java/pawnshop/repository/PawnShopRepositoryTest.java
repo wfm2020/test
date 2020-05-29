d5bee27afadc5b4a1ed676d08f01d5c8b2af2795
Angelos Arampatzis <angelos.arabatzis@gmail.com>
2020-05-29 23:51:08 +0200

    set unique email for each customer

--- src/test/java/pawnshop/repository/PawnShopRepositoryTest.java
+++ src/test/java/pawnshop/repository/PawnShopRepositoryTest.java
@@ -7,7 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import pawnshop.Application;
 import pawnshop.entity.Customer;
+
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -35,12 +37,24 @@ public class PawnShopRepositoryTest {
     }
 
     @Test
-    public void givenCustomer_whenFindByEmail_thenGetOk(){
+    public void givenCustomer_whenFindByEmail_thenGetOk() {
         Customer customer = new Customer("Maxim", "Mustermann", "maxim.mustermann@mail.com",
                 LocalDate.of(1990, 1, 1), "123456");
         customer = customerRepository.save(customer);
         Customer savedCustomer = customerRepository.findByEmail(customer.getEmail());
         assertNotNull(savedCustomer);
         assertEquals("Maxim", savedCustomer.getFirstName());
+    }
+
+    @Test
+    public void givenCustomer_whenSaveExistingEmail_thenGetNotOk() {
+        Customer customer = new Customer("Maxim", "Mustermann", "maxim.mustermann@mail.com",
+                LocalDate.of(1990, 1, 1), "123456");
+        customerRepository.save(customer);
+        Customer duplicateCustomer = new Customer("Maxima", "Musterfrau", "maxim.mustermann@mail.com",
+                LocalDate.of(1991, 2, 2), "012345");
+        customerRepository.save(customer);
+        List<Customer> customers = customerRepository.findAll();
+        assertEquals(1, customers.size());
     }
 }
